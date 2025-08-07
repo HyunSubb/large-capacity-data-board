ed96c4702f8a36e9dc32be339bf7e575c9d7c9c7
신현섭 <didakd22@naver.com>
2025-08-07 20:56:23 +0900

    프로젝트 세팅 완료

---
+++ src/main/java/hyunsub/largecapacitydataboard/LargeCapacityDataBoardApplication.java
@@ -0,0 +1,13 @@
+package hyunsub.largecapacitydataboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeCapacityDataBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeCapacityDataBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyunsub/largecapacitydataboard/LargeCapacityDataBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunsub.largecapacitydataboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeCapacityDataBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
