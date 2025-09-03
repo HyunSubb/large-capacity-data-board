3c1bf83b3a95ba90f49eccbc8e20c55659286d48
신현섭 <didakd22@naver.com>
2025-09-03 20:41:27 +0900

    feat : "인기글 Consumer 구현 - 컨트롤러 & 컨슈머 작성"

---
+++ service/hot-article/src/main/java/hyunsub/board/hotarticle/consumer/HotArticleEventConsumer.java
@@ -0,0 +1,36 @@
+package hyunsub.board.hotarticle.consumer;
+
+import hyunsub.board.hotarticle.service.HotArticleService;
+import kuke.board.common.event.Event;
+import kuke.board.common.event.EventPayload;
+import kuke.board.common.event.EventType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HotArticleEventConsumer {
+    private final HotArticleService hotArticleService;
+
+    /*
+        KafkaListener를 통해서 topic을 구독해서 이벤트를 처리할 수 있다.
+     */
+    @KafkaListener(topics = {
+            EventType.Topic.KUKE_BOARD_ARTICLE,
+            EventType.Topic.KUKE_BOARD_COMMENT,
+            EventType.Topic.KUKE_BOARD_LIKE,
+            EventType.Topic.KUKE_BOARD_VIEW
+    })
+    public void listen(String message, Acknowledgment ack) {
+        log.info("[HotArticleEventConsumer.listen] received message={}", message);
+        Event<EventPayload> event = Event.fromJson(message);
+        if (event != null) {
+            hotArticleService.handleEvent(event);
+        }
+        ack.acknowledge();
+    }
+}
